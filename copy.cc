3336305d41aa8ffdc4f076492f2c33e1ca5d4b11
Machine <machine@MachinedeMacBook-Pro.local>
2023-04-11 23:25:35 +0800

    Support brackets completion

--- copy.cc
+++ copy.cc
@@ -13,6 +13,19 @@ int32_t main() {
   int TESTCASES = 1;
   cin >> TESTCASES;
   while (TESTCASES--) {
+    long long k;
+    cin >> k;
+    vector<int> d;
+    while (k) {
+      d.push_back(k % 9);
+      k /= 9;
+    }
+    reverse(d.begin(), d.end());
+    for (auto& i : d) {
+      if (i >= 4) ++i;
+      cout << i;
+    }
+    cout << '\n';
   }
   return 0;
 }
--- sol.cc
+++ sol.cc
@@ -6,13 +6,26 @@ using namespace std;
 #else
 #define debug(...) 
 #endif
-
+[i][i][j][j][l][i][j][k][i][k][j][j][k][j][j]
 int32_t main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   int TESTCASES = 1;
   cin >> TESTCASES;
   while (TESTCASES--) {
+    long long k;
+    cin >> k;
+    vector<int> d;
+    while (k) {
+      d.push_back(k % 9);
+      k /= 9;
+    }
+    reverse(d.begin(), d.end());
+    for (auto& i : d) {
+      if (i >= 4) ++i;
+      cout << i;
+    }
+    cout << '\n';
   }
   return 0;
 }
