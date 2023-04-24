b57ff43ca727ddef0923308ed2b9e23b5c334be9
Machine <machine@MachinedeMacBook-Pro.local>
2023-04-24 08:06:37 +0800

    sumblime, add find selected text

--- copy.cc
+++ copy.cc
@@ -11,35 +11,61 @@ int32_t main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   int TESTCASES = 1;
-  cin >> TESTCASES;
-  auto ask = [](int r, int c) -> int {
-    cout << "? " << r << ' ' << c << endl;
-    int x;
-    cin >> x;
-    return x;
-  };
   while (TESTCASES--) {
     int n, m;
     cin >> n >> m;
-    int a = ask(1, 1);
-    int b = ask(n, m);
-    int x, y;
-    if (a + b == n - 1) {
-      x = a + 1;
-      y = ask(a + 1, 1) + 1;
-    } else if (a + b == m - 1) {
-      y = a + 1;
-      x = ask(1, a + 1) + 1;
-    } else {
-      if (1 + a <= n and m - b >= 1 and ask(1 + a, m - b) == 0) {
-        x = 1 + a;
-        y = m - b;
+    vector<int> a(n);
+    for (auto& i : a) {
+      cin >> i;
+    }
+    vector<vector<int>> g(n);
+    for (int i = 0; i < n - 1; ++i) {
+      int u, v;
+      cin >> u >> v;
+      --u;
+      --v;
+      g[u].push_back(v);
+      g[v].push_back(u);
+    }
+    vector<int> cnt(n, 1), p(n, -1);
+    vector<long long> sum(n);
+    vector<set<pair<int, int>>> ch(n);
+    function<void(int, int)> dfs = [&](int u, int fa) -> void {
+      sum[u] = a[u];
+      for (auto& v : g[u]) {
+        if (v != fa) {
+          p[v] = u;
+          dfs(v, u);
+          sum[u] += sum[v];
+          cnt[u] += cnt[v];
+          ch[u].emplace(-cnt[v], v);
+        }
+      }
+    };
+    dfs(0, -1);
+    while (m--) {
+      int t, x;
+      cin >> t >> x;
+      --x;
+      if (t == 1) {
+        cout << sum[x] << '\n';
       } else {
-        x = n - b;
-        y = 1 + a;
+        if (ch[x].empty()) {
+          continue;
+        }
+        int c = ch[x].begin()->second;
+        ch[p[x]].erase({-cnt[x], x});
+        ch[x].erase({-cnt[c], c});
+        sum[x] -= sum[c];
+        sum[c] += sum[x];
+        cnt[x] -= cnt[c];
+        cnt[c] += cnt[x];
+        ch[p[x]].emplace(-cnt[c], c);
+        ch[c].emplace(-cnt[x], x);
+        p[c] = p[x];
+        p[x] = c;
       }
     }
-    cout << "! " << x << ' ' << y << endl;
   }
   return 0;
 }
