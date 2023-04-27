ed61e7404b5a11bed7a30df8d07379ad0805907c
jackcai <you@example.com>
2023-04-27 12:39:37 +0800

    update todo.

--- copy.cc
+++ copy.cc
@@ -7,137 +7,35 @@ using namespace std;
 #define debug(...) 
 #endif
 
-static inline void lowStr(string& s) {
-  for (auto& i : s) {
-    i = char(tolower(i));
-  }
-}
-
-static inline void upStr(string& s) {
-  for (auto& i : s) {
-    i = char(toupper(i));
-  }
-}
-
-static inline void readSortedPair(vector<pair<string, string>>& csp, int& sz) {
-  string s;
-
-  while (getline(cin, s)) {
-    if (s.find("TWFCD") != string::npos) break;
-    csp.emplace_back("", s);
-  }
-
-  sz = (int) csp.size();
-  int ix = 0;
-  cout << "Count = " << sz << '\n';
-  
-  csp[ix] = {csp[ix].second, s + ", " + csp[ix].second};
-  ++ix;
-  while (getline(cin, s)) {
-    csp[ix] = {csp[ix].second, s + ", " + csp[ix].second};
-    ++ix;
-  }
-  sort(csp.begin(), csp.end(), [&](const pair<string, string>& lhs, const pair<string, string>& rhs) {
-    string l = lhs.first;
-    string r = rhs.first;
-    lowStr(l);
-    lowStr(r);
-    return l < r;
-  });
-}
-
-static inline void getList(vector<pair<string, string>>& csp, vector<string>& cs, int& sz) {
-  for (int i = 0; i < sz; ++i) {
-    cs[i] = csp[i].second;
-  }
-}
-
-static inline void getSplit(string& i, vector<string>& sp) {
-  string ss;
-  stringstream x(i); 
-
-  while (getline(x, ss, ',')) {
-    sp.push_back(ss);
-  }  
-}
-
-static inline void upProName(string& i, string& ii, vector<string>& sp) {
-  auto it = ii.find(sp[1]);
-  for (int k = 0; k < (int) sp[1].size(); ++k) {
-    i[it + k] = char(toupper(i[it + k])); 
-  }
-}
-
-static inline void classify(const vector<string>& pdline, vector<string>& cs, map<string, vector<string>>& all, int& sz) {
-  for (auto& i : cs) {
-    vector<string> sp;
-    getSplit(i, sp);
-
-    bool ok = false;
-    lowStr(sp[1]);
- 
-    for (auto& jj : pdline) {
-      string j = jj;
-      lowStr(j);
- 
-      if (sp[1].find(j) != string::npos) {
-        string ii = i;
-        lowStr(ii);
-
-        upProName(i, ii, sp);
-        all[j].push_back(i);
-
-        --sz;
-        ok = true;
-        break;
-      }
-    }
-    assert(ok && "No productline match\n");
-  }
-}
-
-static inline void outP(const map<string, vector<string>>& all) {
-  for (auto& [x, y] : all) {
-    string xx = x;
-    upStr(xx);
-    cout << xx << '\n';
- 
-    for (auto& z : y) {
-      cout << z << '\n';
-    }
-    cout << '\n';
-  }
-}
-
-static inline void shoudZero(int& sz) {
-  assert(!sz && "Should be Zero\n");
-}
-
-
-static inline void genReport() {
-  const vector<string> pdline{"uisp","usw", "usp", "uled", "uacc", "uis", "usc"};
-
-  int sz = 0;
-  map<string, vector<string>> all;
-  vector<pair<string, string>> csp;
-  vector<string> cs;
-  
-  readSortedPair(csp, sz);
-  cs.resize(sz);
-  getList(csp, cs, sz);
-
-  classify(pdline, cs, all, sz);
-  shoudZero(sz);  
-  outP(all);
-}
-
 int32_t main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-
   int TESTCASE = 1;
   while (TESTCASE--) {
-    genReport();
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> a(m), b(m), c(m);
+    for (int i = 0; i < m; ++i) {
+      cin >> a[i] >> b[i] >> c[i];
+      --a[i];
+      --b[i];
+    }
+    vector<int> e(k);
+    for (auto& i : e) {
+      cin >> i;
+      --i;
+    }
+    const long long inf = 1e18;
+    vector<long long> dp(n, inf);
+    dp[0] = 0;
+    for (int i = 0; i < k; ++i) {
+      int j = e[i];
+      dp[b[j]] = min(dp[b[j]], dp[a[j]] + 1ll * c[j]);
+    }
+    if (dp[n - 1] >= inf) {
+      dp[n - 1] = -1;
+    }
+    cout << dp[n - 1] << '\n';
   }
   return 0;
 }
