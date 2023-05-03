8ea14529e824291d14b49d0a57dd55f9d2b8b2f0
Machine <machine@MachinedeMacBook-Pro.local>
2023-05-03 23:49:43 +0800

    update light vimrc.

---
+++ lib/sol.cc
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int isWinner(vector<int>& player1, vector<int>& player2) {
+      int n = player1.size();
+      int a = 0;
+      for (int i = 0, c = 0; i < n; ++i) {
+        if (c) {
+          a += 2 * player1[i];
+          --c;
+        } else {
+          a += player1[i];
+        }
+        if (player1[i] == 10) {
+          c = 2;
+        }
+      }
+      int b = 0;
+      for (int i = 0, c = 0; i < n; ++i) {
+        if (c) {
+          b += 2 * player2[i];
+          --c;
+        } else {
+          b += player2[i];
+        }
+        if (player2[i] == 10) {
+          c = 2;
+        }
+      }
+      if (a > b) {
+        return 1;
+      } else if (a < b) {
+        return 2;
+      }
+      return 0;
+    }
+};
--- sol.cc
+++ sol.cc
@@ -1,18 +1,47 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-#ifdef LOCAL
-#include"lib/debug.h"
-#else
-#define debug(...) 
-#endif
-
-int32_t main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int TESTCASES = 1;
-  cin >> TESTCASES;
-  while (TESTCASES--) {
-  }
-  return 0;
-}
+class Solution {
+public:
+    long long countOperationsToEmptyArray(vector<int>& nums) {
+      int n = nums.size();
+      vector<long long> f(n + 1);
+      auto add = [&](int x, int v) -> void {
+        for (int i = x; i <= n; i += (i & -i)) {
+          f[i] += 1ll * v;
+        }
+      };
+      auto get = [&](int x) -> long long {
+        long long ans = 0;
+        for (int i = 0; i < n; ++i) {
+        }
+        for (int i = 0; i < n; ++i) {
+        }
+        auto sum = [&](int x, int y) {
+          return get(y) - get(x - 1);
+        };
+        for (int i = x; i >= 1; i -= (i & -i)) {
+          ans += 1ll * f[i];
+        }
+        return ans;
+      };
+      auto sum = [&](int x, int y) -> long long {
+        return get(y) - get(x - 1);
+      };
+      vector<pair<int, int>> p(n);
+      for (int i = 0; i < n; ++i) {
+        p[i] = {nums[i], i + 1};
+        add(i + 1);
+      }
+      long long ans = 0;
+      sort(p.begin(), p.end());
+      int lst = 0;
+      for (auto& [_, i] : p) {
+        if (lst > i) {
+          ans += get(lst, n) + get(1, i);
+        } else {
+          ans += get(lst, i);
+        }
+        lst = i;
+        add(i, -1);
+      }
+      return ans;
+    }
+};
