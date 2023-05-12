77c59ed311a011bb92c1b021dea4dc36ae4afab5
jackcai <you@example.com>
2023-05-12 20:56:26 +0800

    update todo

--- sol.cc
+++ sol.cc
@@ -11,61 +11,18 @@ int32_t main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   int TESTCASE = 1;
+  cin >> TESTCASE;
   while (TESTCASE--) {
     int n;
     cin >> n;
-    vector<vector<int>> g(n);
-    vector<int> d(n);
-    for (int i = 0; i < n; ++i) {
-      int u, v;
-      cin >> u >> v;
-      --u;
-      --v;
-      g[u].push_back(v);
-      g[v].push_back(u);
-      ++d[u];
-      ++d[v];
-    }
-    vector<int> q;
-    for (int i = 0; i < n; ++i) {
-      if (d[i] == 1) {
-        q.push_back(i);
-      }
-    }
-    vector<bool> vis(n);
-    for (int i = 0; i < (int) q.size(); ++i) {
-      auto u = q[i];
-      vis[u] = 1;
-      for (auto& v : g[u]) {
-        --d[v];
-        if (d[v] == 1) {
-          q.push_back(v);
+    for (int i = 1; i <= n; ++i) {
+      cout << i << ' ';
+      for (int j = n; j >= 1; --j) {
+        if (j != i) {
+          cout << j << ' ';
         }
       }
-    }
-    vector<int> c(n);
-    function<void(int, int, int)> dfs = [&](int u, int fa, int co) {
-      c[u] = co;
-      for (auto& v : g[u]) {
-        if (v == fa or !vis[v]) {
-          continue;
-        }
-        dfs(v, u, co);
-      }
-    };
-    for (int i = 0; i < n; ++i) {
-      if (!vis[i]) {
-        dfs(i, -1, i + 1);
-      }
-    }
-    int qq;
-    cin >> qq;
-    while (qq--) {
-      int u, v;
-      cin >> u >> v;
-      --u;
-      --v;
-      cout << (c[u] == c[v] ? "Yes" : "No") << '\n';
+      cout << '\n';
     }
   }
   return 0;
