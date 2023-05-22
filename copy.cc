3c43c22e272a41f334a5d5707b87cd404be62c0e
jackcai <you@example.com>
2023-05-22 11:12:01 +0800

    update light vimrc

--- copy.cc
+++ copy.cc
@@ -13,6 +13,15 @@ int32_t main() {
   int TESTCASE = 1;
   cin >> TESTCASE;
   while (TESTCASE--) {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    set<pair<char, char>> vis;
+    for (int i = 0; i + 1 < n; ++i) {
+      vis.emplace(s[i], s[i + 1]);
+    }
+    cout << vis.size() << '\n';
   }
   return 0;
 }
