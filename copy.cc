c797d66f7d660159293a829993d0173330d34905
jackcai <you@example.com>
2023-06-02 18:43:20 +0800

    update reset.cc

--- copy.cc
+++ copy.cc
@@ -12,25 +12,26 @@ int32_t main() {
   ios::sync_with_stdio(false), cin.tie(0);
 
   auto solve = [&]() {
-    long long n;
-    int k;
-    cin >> n >> k;
-    const long long inf = 1e18;
-    long long mn = inf;
-    long long type = 1;
-    long long cnt = 0;
-    for (int i = 1; i <= k; ++i) {
-      long long t; cin >> t;
-      if (t > n) {
-         continue;
-      }
-      if (n % t < mn) {
-        mn = n % t;
-        type = i;
-        cnt = n / t;
-      }
+    int n; cin >> n;
+    vector<int> x(5 + 1);
+    vector<int> y(5 + 1);
+    for (int i = 0; i < n; ++i) {
+      int t; cin >> t;
+      ++x[t];
     }
-    cout << type << ' ' << cnt << '\n';
+    for (int i = 0; i < n; ++i) {
+      int t; cin >> t;
+      ++y[t];
+    }
+    int ans = 0;
+    for (int i = 1; i <= 5; ++i) {
+      if ((x[i] + y[i]) & 1) {
+        cout << -1 << '\n';
+        return;
+      }
+      ans += abs(x[i] - (x[i] + y[i]) / 2);
+    }
+    cout << ans / 2 << '\n';
   };
 
   {
--- sol.cc
+++ sol.cc
@@ -12,29 +12,12 @@ int32_t main() {
   ios::sync_with_stdio(false), cin.tie(0);
 
   auto solve = [&]() {
-    long long n;
-    int k;
-    cin >> n >> k;
-    const long long inf = 1e18;
-    long long mn = inf;
-    long long type = 1;
-    long long cnt = 0;
-    for (int i = 1; i <= k; ++i) {
-      long long t; cin >> t;
-      if (t > n) {
-         continue;
-      }
-      if (n % t < mn) {
-        mn = n % t;
-        type = i;
-        cnt = n / t;
-      }
-    }
-    cout << type << ' ' << cnt << '\n';
+
   };
 
   {
     int TC = 1;
+    cin >> TC;
 
     for (int i = 1; i <= TC; ++i) {
       solve();
