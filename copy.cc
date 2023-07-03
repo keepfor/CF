8d34dab30d2c36aa981890c22942cbad4111b41c
jackcai <you@example.com>
2023-07-03 08:13:27 +0800

    monday

--- copy.cc
+++ copy.cc
@@ -12,30 +12,24 @@ int32_t main() {
   ios::sync_with_stdio(false), cin.tie(0);
 
   auto solve = [&]() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    long long s = 0;
-    for (auto& i : a) {
-      cin >> i;
-      s += 1ll * abs(i);
-    }
-    int ans = 0;
-    for (int i = 0; i < n; ++i) {
-      if (a[i] >= 0) continue;
-      int j = i;
-      while (j < n and a[j] <= 0) {
-        ++j;
+    int n; cin >> n;
+    vector<int> a(n); for (auto& i : a) cin >> i;
+    int t = min(n, 2);
+    int ans = t;
+
+    for (int i = 2; i < n; ++i) {
+      if (a[i] == a[i - 1] + a[i - 2]) {
+        ++t;
+      } else {
+        t = 2;
       }
-      ++ans;
-      i = j - 1;
+      ans = max(ans, t);
     }
-    cout << s << ' ' << ans << '\n';
+    cout << ans << '\n';
   };
 
   {
     int tt = 1;
-    cin >> tt;
     while (tt--) {
       solve();
     }
--- sol.cc
+++ sol.cc
@@ -12,30 +12,11 @@ int32_t main() {
   ios::sync_with_stdio(false), cin.tie(0);
 
   auto solve = [&]() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    long long s = 0;
-    for (auto& i : a) {
-      cin >> i;
-      s += 1ll * abs(i);
-    }
-    int ans = 0;
-    for (int i = 0; i < n; ++i) {
-      if (a[i] >= 0) continue;
-      int j = i;
-      while (j < n and a[j] <= 0) {
-        ++j;
-      }
-      ++ans;
-      i = j - 1;
-    }
-    cout << s << ' ' << ans << '\n';
+
   };
 
   {
     int tt = 1;
-    cin >> tt;
     while (tt--) {
       solve();
     }
