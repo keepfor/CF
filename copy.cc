2cff640fc9c9ee7050abedc7b73ccc988aaeb8ef
jackcai <you@example.com>
2023-07-10 21:37:12 +0800

    add progess

--- copy.cc
+++ copy.cc
@@ -12,50 +12,39 @@ int32_t main() {
   ios::sync_with_stdio(false), cin.tie(0);
 
   auto solve = [&]() {
-    int n, m; cin >> n >> m;
-    vector<vector<pair<int, long long>>> g(n);
+    string s;
+    cin >> s;
+    int n = (int) s.size();
+    vector<vector<int>> nx(n + 1, vector<int>(10, n));
+    for (int i = n - 1; i >= 0; --i) {
+      nx[i] = nx[i + 1];
+      nx[i][s[i] - '0'] = i;
+    }
+      
+    int m;
+    cin >> m;
+    string x, y;
+    cin >> x >> y;
+
+    int p = 0;
     for (int i = 0; i < m; ++i) {
-      int u, v;
-      long long w;
-      cin >> u >> v >> w;
-      --u; --v;
-      g[u].emplace_back(v, w);
-      g[v].emplace_back(u, w);
-    }
-    int k; cin >> k;
-    vector<int> ans(n, -1);
-    priority_queue<array<long long, 3>, vector<array<long long, 3>>, greater<>> q;
-    for (int i = 0; i < k; ++i) {
-      int a; cin >> a; --a;
-      ans[a] = 0;
-      for (auto& [v, w] : g[a]) {
-        q.push({w, v, -1});
+      int l = x[i] - '0';
+      int r = y[i] - '0';
+      int pp = p;
+      for (int j = l; j <= r; ++j) {
+        pp = max(pp, nx[p][j]);
+        if (pp >= n) break;
       }
+      p = pp + 1;
+      if (p >= n + 1) break;
     }
-    int d; cin >> d;
-    for (int i = 0; i < d; ++i) {
-      long long x; cin >> x;
-      vector<array<long long, 3>> nex;
-      while (q.size() and q.top()[0] <= x) {
-        auto [w, u, t] = q.top(); q.pop();
-        if (t != -1 and t < i) continue;
-        if (ans[u] != -1) continue;
-        ans[u] = 1 + i;
-        for (auto& [v, ww] : g[u]) {
-          if (ans[v] != -1) continue;
-          q.push({w + ww, v, i});
-          nex.push_back({ww, v, -1});
-        }
-      }
-      for (auto& t : nex) q.push(t);
-    }
-    for (int i = 0; i < n; ++i) {
-      cout << ans[i] << "\n";
-    }
+    if (p >= n + 1) cout << "YES\n";
+    else cout << "NO\n";
   };
 
   {
     int tt = 1;
+    cin >> tt;
     while (tt--) {
       solve();
     }
