1dc16fe371b3e9527114f98dd85d17341c435cd8
jackcai <you@example.com>
2023-08-29 13:49:53 +0800

    update todo

--- sol.cc
+++ sol.cc
@@ -1,50 +1,144 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#ifdef DEBUG
-#include "lib/debug.cc"
+#ifdef LOCAL
+#include "./lib/debug.h"
 #else
-#define debug(...) 0
+#define debug(...)
 #endif
 
+static inline void lowStr(string& s) {
+  for (auto& i : s) {
+    i = char(tolower(i));
+  }
+}
+
+static inline void upStr(string& s) {
+  for (auto& i : s) {
+    i = char(toupper(i));
+  }
+}
+
+static inline void readSortedPair(vector<pair<string, string>>& csp, int& sz) {
+  string s;
+
+  while (getline(cin, s)) {
+    if (s.find("TWFCD") != string::npos) break;
+    csp.emplace_back("", s);
+  }
+
+  sz = (int)csp.size();
+  int ix = 0;
+  cout << "Count = " << sz << '\n';
+
+  csp[ix] = {csp[ix].second, s + ", " + csp[ix].second};
+  ++ix;
+  while (getline(cin, s)) {
+    csp[ix] = {csp[ix].second, s + ", " + csp[ix].second};
+    ++ix;
+  }
+  sort(csp.begin(), csp.end(),
+       [&](const pair<string, string>& lhs, const pair<string, string>& rhs) {
+         string l = lhs.first;
+         string r = rhs.first;
+         lowStr(l);
+         lowStr(r);
+         return l < r;
+       });
+}
+
+static inline void getList(vector<pair<string, string>>& csp,
+                           vector<string>& cs, int& sz) {
+  for (int i = 0; i < sz; ++i) {
+    cs[i] = csp[i].second;
+  }
+}
+
+static inline void getSplit(string& i, vector<string>& sp) {
+  string ss;
+  stringstream x(i);
+
+  while (getline(x, ss, ',')) {
+    sp.push_back(ss);
+  }
+}
+
+static inline void upProName(string& i, string& ii, vector<string>& sp) {
+  auto it = ii.find(sp[1]);
+  for (int k = 0; k < (int)sp[1].size(); ++k) {
+    i[it + k] = char(toupper(i[it + k]));
+  }
+}
+
+static inline void classify(const vector<string>& pdline, vector<string>& cs,
+                            map<string, vector<string>>& all, int& sz) {
+  for (auto& i : cs) {
+    vector<string> sp;
+    getSplit(i, sp);
+
+    bool ok = false;
+    lowStr(sp[1]);
+
+    for (auto& jj : pdline) {
+      string j = jj;
+      lowStr(j);
+
+      if (sp[1].find(j) != string::npos) {
+        string ii = i;
+        lowStr(ii);
+
+        upProName(i, ii, sp);
+        all[j].push_back(i);
+
+        --sz;
+        ok = true;
+        break;
+      }
+    }
+    assert(ok && "No productline match");
+  }
+}
+
+static inline void outP(const map<string, vector<string>>& all) {
+  for (auto& [x, y] : all) {
+    string xx = x;
+    upStr(xx);
+    cout << xx << '\n';
+
+    for (auto& z : y) {
+      cout << z << '\n';
+    }
+    cout << '\n';
+  }
+}
+
+static inline void shoudZero(int& sz) { assert(!sz && "Should be Zero\n"); }
+
+static inline void genReport() {
+  const vector<string> pdline{"uisp", "usw", "usp", "uled",
+                              "uacc", "uis", "usc"};
+
+  int sz = 0;
+  map<string, vector<string>> all;
+  vector<pair<string, string>> csp;
+  vector<string> cs;
+
+  readSortedPair(csp, sz);
+  cs.resize(sz);
+  getList(csp, cs, sz);
+
+  classify(pdline, cs, all, sz);
+  shoudZero(sz);
+  outP(all);
+}
+
 int32_t main() {
-  ios::sync_with_stdio(false), cin.tie(0);
+  ios::sync_with_stdio(false);
+  cin.tie(0);
 
-  auto solve = [&]() {
-    using ll = long long;
-    int n;
-    cin >> n;
-    vector<ll> v(n);
-    for (auto& i : v) cin >> i;
-    ll mx = *max_element(v.begin(), v.end());
-    ll mn = *min_element(v.begin(), v.end());
-    ll d = (mx - mn + n - 1) / n;
-    if (!d) {
-      cout << 0 << '\n';
-      return;
-    }
-    vector<vector<ll>> b(n);
-    ll ans = 0;
-    ll pre;
-    for (auto& i : v) {
-      int x = (i - mn) / d;
-      if (x == n) --x;
-      b[x].push_back(i);
-    }
-    for (int i = 0; i < n; ++i) {
-      if (b[i].empty()) continue;
-      ll mx = *max_element(b[i].begin(), b[i].end());
-      ll mn = *min_element(b[i].begin(), b[i].end());
-      if (i) ans = max(ans, mn - pre);
-      pre = mx;
-    }
-    cout << ans << '\n';
-  };
-
-  {
-    int tt = 1;
-    while (tt--) solve();
+  int TESTCASE = 1;
+  while (TESTCASE--) {
+    genReport();
   }
   return 0;
 }
