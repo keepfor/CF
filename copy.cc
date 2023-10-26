c7c2ee22ff41424c0018ca547e8ee48f4f282abc
jackcai <you@example.com>
2023-10-26 18:44:20 +0800

    add vim nu

--- copy.cc
+++ copy.cc
@@ -8,14 +8,55 @@ using namespace std;
 #define debug(...) 0
 #endif
 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
 int32_t main() {
   auto SetIO = []() -> void { cin.tie(0)->sync_with_stdio(0); };
   SetIO();
   auto SolveOne = []() -> void {
-    set<vector<int>> v;
-    vector<int> a{1, 2, 3};
-    vector<int> b{1, 2, 3};
-    v.insert(a);
+    const int n = 11;
+    vector<int> v(n);
+    for (auto& i : v) {
+      i += rng() % 100;
+    }
+    debug(v);
+    auto q = [&](int l, int r) -> int {
+      int x = v[l];
+      while (1) {
+        while (v[l] < x) {
+          ++l;
+        }
+        while (v[r] > x) {
+          --r;
+        }
+        if (l >= r) {
+          debug(l, r);
+          return r;
+        }
+        swap(v[l], v[r]);
+        debug(v);
+        ++l;
+        --r;
+      };
+    };
+    int l = 0;
+    int r = n - 1;
+    int k = 2;
+    while (l <= r) {
+      int p = q(l, r);
+      debug(p);
+      if (p == k) {
+        for (auto& i : v) {
+          cout << i << ' ';
+        }
+        cout << '\n';
+        break;
+      } else if (p < k) {
+        l = p + 1;
+      } else {
+        r = p - 1;
+      }
+    }
   };
   auto Solve = [&SolveOne]() -> void {
     int tt = 1;
