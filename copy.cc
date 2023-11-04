b857dc0037baff08fc46c1d4d9c02cc668868c27
Machine <machine@MachinedeMacBook-Pro.local>
2023-11-04 11:15:49 +0800

    update

--- copy.cc
+++ copy.cc
@@ -7,38 +7,27 @@ using namespace std;
 #else
 #define debug(...) 0
 #endif
-bool isPossible(vector<int>& A) {
-  unordered_map<int, int> left, end;
-  for (int i : A) {
-    left[i]++;
-  }
-  for (int i : A) {
-    cout << i << ',';
-    if (left[i] == 0) continue;
-    left[i]--;
-    if (end[i - 1] > 0) {
-      end[i - 1]--;
-      end[i]++;
-    } else if (left[i + 1] > 0 && left[i + 2] > 0) {
-      left[i + 1]--;
-      left[i + 2]--;
-      end[i + 2]++;
-    } else {
-      return false;
-    }
-  }
-  return true;
-}
+
 int32_t main() {
   auto SetIO = []() -> void { cin.tie(0)->sync_with_stdio(0); };
   SetIO();
   auto SolveOne = []() -> void {
-    vector<int> a{4, 4, 3, 3, 2, 2, 1, 1};
-    sort(a.begin(), a.end());
-    debug(isPossible(a));
+    int n;
+    cin >> n;
+    vector<int> v(2 * n);
+    for (auto& i : v) {
+      cin >> i;
+    }
+    sort(v.begin(), v.end());
+    int ans = 0;
+    for (int i = 0; i < n; i += 2) {
+      ans += v[i + 1] - v[i];
+    }
+    cout << ans << '\n';
   };
   auto Solve = [&SolveOne]() -> void {
     int t = 1;
+    cin >> t;
     while (t--) {
       SolveOne();
     }
