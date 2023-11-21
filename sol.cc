bd0456f237ab393c1357c6bd065134a089b60856
jack <you@example.com>
2023-11-21 08:18:00 +0800

    todo

--- sol.cc
+++ sol.cc
@@ -12,27 +12,29 @@ int32_t main() {
   auto SetIO = []() -> void { cin.tie(0)->sync_with_stdio(0); };
   SetIO();
   auto SolveOne = []() -> void {
-    int n;
-    cin >> n;
-    using ll = long long;
-    vector<ll> a(n);
+    int n, x, y;
+    cin >> n >> x >> y;
+    vector<int> a(n);
     for (auto& i : a) {
       cin >> i;
     }
-    sort(a.begin(), a.end());
-    const ll inf = 1e18;
-    ll v = inf;
-    ll cnt = 0;
-    for (int i = 0; i + 1 < n; ++i) {
-      ll d = a[i + 1] - a[i];
-      if (d < v) {
-        v = d;
-        cnt = 1;
-      } else if (d == v) {
-        ++cnt;
+    sort(a.rbegin(), a.rend());
+    if (x > y) {
+      swap(x, y);
+    }
+    double xs = 0;
+    double ys = 0;
+
+    for (int i = 0; i < x + y; ++i) {
+      if (i < x) {
+        xs += a[i];
+      } else {
+        ys += a[i];
       }
     }
-    cout << v << ' ' << cnt << '\n';
+    xs /= x;
+    ys /= y;
+    cout << fixed << setprecision(10) << xs + ys << '\n';
   };
   auto Solve = [&SolveOne]() -> void {
     int tt = 1;
