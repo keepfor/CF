213a3ef6addd3009c387748d27ff958e0eb3909b
jack <you@example.com>
2024-02-22 10:37:14 +0800

    ad digit trie

--- lib/trie.h
+++ lib/trie.h
@@ -116,3 +116,52 @@ public:
         return ans;
     }
 };
+namespace digit {
+template <class T>
+struct Trie {
+  vector<vector<int>> trie{vector<int>(10, -1)};
+
+  void newNode() { trie.emplace_back(10, -1); }
+
+  void add(string s) {
+    int now = 0;
+    for (auto& i : s) {
+      int x = i - '0';
+      if (trie[now][x] == -1) {
+        trie[now][x] = trie.size();
+        newNode();
+      }
+      now = trie[now][x];
+    }
+  }
+
+  T lcp(string s) {
+    int len = 0;
+    int now = 0;
+    for (auto& i : s) {
+      int x = i - '0';
+      if (trie[now][x] == -1) {
+        break;
+      }
+      ++len;
+      now = trie[now][x];
+    }
+    return len;
+  }
+};
+
+class Solution {
+ public:
+  int longestCommonPrefix(vector<int>& v, vector<int>& b) {
+    Trie<int> t;
+    int ans = 0;
+    for (auto& i : v) {
+      t.add(to_string(i));
+    }
+    for (auto& i : b) {
+      ans = max(ans, t.lcp(to_string(i)));
+    }
+    return ans;
+  }
+};
+}
