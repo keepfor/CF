4d9d546c019f4f9a75e87378a04ffb2a069d4002
jack <you@example.com>
2024-03-16 20:00:53 +0800

    upd to

--- sol.cc
+++ sol.cc
@@ -10,46 +10,11 @@ using namespace std;
 
 using ll = long long;
 
-void Solve() {
-  int n, m, x;
-  cin >> n >> m >> x;
-  vector<bool> dp(n);
-  dp[x - 1] = true;
-  for (int i = 0; i < m; ++i) {
-    vector<bool> ndp(n);
-    int r;
-    char c;
-    cin >> r >> c;
-    for (int j = 0; j < n; ++j) {
-      if (!dp[j]) {
-        continue;
-      }
-      if (c == '0' or c == '?') {
-        ndp[(j + r) % n] = 1;
-      }
-      if (c == '1' or c == '?') {
-        ndp[(n + j - r) % n] = 1;
-      }
-    }
-
-    swap(dp, ndp);
-  }
-  vector<int> ans;
-  for (int i = 0; i < n; ++i) {
-    if (dp[i]) {
-      ans.push_back(i);
-    }
-  }
-  const int z = ans.size();
-  cout << z << '\n';
-  for (int i = 0; i < z; ++i) {
-    cout << ans[i] + 1 << " \n"[i + 1 == z];
-  }
-}
+void Solve() {}
 
 void Main() {
   auto t{1};
-  cin >> t;
+  // cin >> t;
   while (t--) {
     Solve();
   }
@@ -61,5 +26,4 @@ signed main() {
   SetIO();
   Main();
   return 0;
-
-
+}
