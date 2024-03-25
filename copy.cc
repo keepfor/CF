551f09b5910ae6fd6951fe9522ae48fd23096a8b
jack <you@example.com>
2024-03-25 10:21:37 +0800

    add trie get max lcsuf

--- copy.cc
+++ copy.cc
@@ -8,28 +8,112 @@ using namespace std;
 #define debug(...)
 #endif
 
-struct Solver {
-  using ll = long long;
+template <int Size>
+class Trie {
+ public:
+  Trie() { New(); }
 
-  void Solve() {
-    cout << "Hello\n";
-  }
-
-  void Run() {
-    auto t{1};
-    cin >> t;
-    while (t--) {
-      Solve();
+  void Insert(string s, int idx) {
+    int x = 0;
+    id[0] = idx;
+    for (auto& i : s) {
+      int c = i - 'a';
+      if (t[x][c] == -1) {
+        t[x][c] = t.size();
+        New();
+      }
+      id[t[x][c]] = idx;
+      x = t[x][c];
     }
   }
 
-  inline void SetIO() { cin.tie(0)->sync_with_stdio(0); }
+  int GetLCSuf(string s) {
+    int x = 0;
+    for (auto& i : s) {
+      int c = i - 'a';
+      if (t[x][c] == -1) {
+        return id[x];
+      }
+      x = t[x][c];
+    }
+    return id[x];
+  }
 
-  void Main() {
+ private:
+  static constexpr int n = Size;
+
+  void New() {
+    t.emplace_back(vector<int>(n, -1));
+    id.push_back(-1);
+  }
+
+  vector<vector<int>> t;
+  vector<int> id;
+};
+
+class Solution {
+ public:
+  vector<int> stringIndices(vector<string>& v, vector<string>& q) {
+    for (auto& i : v) {
+      reverse(i.begin(), i.end());
+    }
+    for (auto& i : q) {
+      reverse(i.begin(), i.end());
+    }
+    const int n = v.size();
+    vector<int> ord(n);
+    iota(ord.begin(), ord.end(), 0);
+    sort(ord.begin(), ord.end(),
+         [&](auto a, auto b) -> bool { return v[a].size() < v[b].size(); });
+    Trie<26> t;
+    for (int i = n - 1; i >= 0; --i) {
+      t.Insert(v[ord[i]], ord[i]);
+    }
+    const int m = q.size();
+    vector<int> ans(m);
+    for (int i = 0; i < m; ++i) {
+      ans[i] = t.GetLCSuf(q[i]);
+    }
+    return ans;
+  }
+};
+
+class Solver {
+ public:
+  inline void Main() const {
     SetIO();
     Run();
   }
+
+ private:
+  using ll = long long;
+
+  inline void SetIO() const { cin.tie(0)->sync_with_stdio(0); }
+
+  void Solve() const;
+  void Run() const;
 };
+
+void Solver::Solve() const {
+  Solution s;
+  /*
+   *
+   *
+Input: wordsContainer = [], wordsQuery = []
+[], wordsQuery = []
+   * */
+  vector<string> a{"abcdefgh","poiuygh","ghghgh"};
+  vector<string> b{"gh","acbfgh","acbfegh"};
+  debug(s.stringIndices(a, b));
+}
+
+void Solver::Run() const {
+  auto tt{1};
+  // cin >> tt;
+  while (tt--) {
+    Solve();
+  }
+}
 
 signed main() {
   Solver solver;
--- lib/trie.h
+++ lib/trie.h
@@ -1,7 +1,3 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
 class Trie {
  public:
   vector<vector<int>> trie;
@@ -165,3 +161,76 @@ class Solution {
   }
 };
 }
+namespace longest_common_suffix_queries{
+//https://leetcode.com/problems/longest-common-suffix-queries/
+template <int Size>
+class Trie {
+ public:
+  Trie() { New(); }
+
+  void Insert(string s, int idx) {
+    int x = 0;
+    id[0] = idx;
+    for (auto& i : s) {
+      int c = i - 'a';
+      if (t[x][c] == -1) {
+        t[x][c] = t.size();
+        New();
+      }
+      x = t[x][c];
+      id[x] = idx;
+    }
+  }
+
+  int GetLCSuf(string s) {
+    int x = 0;
+    for (auto& i : s) {
+      int c = i - 'a';
+      if (t[x][c] == -1) {
+        return id[x];
+      }
+      x = t[x][c];
+    }
+    return id[x];
+  }
+
+ private:
+  static constexpr int n = Size;
+
+  void New() {
+    t.emplace_back(vector<int>(n, -1));
+    id.push_back(-1);
+  }
+
+  vector<vector<int>> t;
+  vector<int> id;
+};
+
+class Solution {
+ public:
+  vector<int> stringIndices(vector<string>& v, vector<string>& q) {
+    for (auto& i : v) {
+      reverse(i.begin(), i.end());
+    }
+    for (auto& i : q) {
+      reverse(i.begin(), i.end());
+    }
+    const int n = v.size();
+    vector<int> ord(n);
+    iota(ord.begin(), ord.end(), 0);
+    stable_sort(ord.begin(), ord.end(), [&](auto a, auto b) -> bool {
+      return v[a].size() < v[b].size();
+    });
+    Trie<26> t;
+    for (int i = n - 1; i >= 0; --i) {
+      t.Insert(v[ord[i]], ord[i]);
+    }
+    const int m = q.size();
+    vector<int> ans(m);
+    for (int i = 0; i < m; ++i) {
+      ans[i] = t.GetLCSuf(q[i]);
+    }
+    return ans;
+  }
+};
+};
