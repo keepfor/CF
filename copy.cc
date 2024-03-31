6ad4b2dabbc88fe1288cf592c31cd1b0c938ce00
mac <machine@test.local>
2024-03-31 12:54:06 +0800

    upd zshrc

--- copy.cc
+++ copy.cc
@@ -8,30 +8,45 @@ using namespace std;
 #define debug(...)
 #endif
 
+using ll = long long;
+
+class Solution {
+public:
+    int minimumDistance(vector<vector<int>>& points) {
+      return 0;
+    }
+};
+
 class Solver {
  public:
-  void Main() {
+  inline void Main() const {
     SetIO();
     Run();
   }
 
  private:
-  using ll = long long;
-
-  inline void SetIO() const { cin.tie(0)->sync_with_stdio(0); }
-
-  void Solve() const;
-
-  void Run() const {
-    auto tt{1};
-    // cin >> tt;
-    while (tt--) {
-      Solve();
-    }
+  inline void SetIO() const {
+    cin.tie(0)->sync_with_stdio(0);
   }
+  void Solve() const;
+  void Run() const;
 };
 
-void Solver::Solve() const {}
+using ll = long long;
+
+void Solver::Solve() const {
+  Solution s;
+  vector<vector<int>> t{{3,10},{5,15},{10,2},{4,4}};
+  debug(s.minimumDistance(t));
+}
+
+void Solver::Run() const {
+  auto tt{1};
+  //cin >> tt;
+  while (tt--) {
+    Solve();
+  }
+}
 
 signed main() {
   Solver solver;
