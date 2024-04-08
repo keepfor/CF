1c74db79dcdf28b7e21cf31071c1c8c06a742ec5
jack <you@example.com>
2024-04-08 12:15:08 +0800

    update vimrc

--- copy.cc
+++ copy.cc
@@ -26,13 +26,53 @@ class Solver {
 using ll = long long;
 
 void Solver::Solve() const {
-  vector<bool> a(1);
-  debug(a[0]);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (auto& i : a) {
+    cin >> i;
+  }
+  vector<int> b(n);
+  for (auto& i : b) {
+    cin >> i;
+  }
+  set<pair<ll, ll>> vis;
+  ll sum = 0;
+  ll de = 0;
+  for (int i = 0; i < n; ++i) {
+    sum += a[i] - b[i];
+    ll mx = a[i];
+    ll mn = b[i];
+    assert(b[i] <= a[i]);
+    while (1) {
+      auto it = vis.lower_bound({mn, mx});
+      if (it != vis.end() and it->first <= mx) {
+        mx = max(mx, it->second);
+        vis.erase(it);
+      } else {
+        break;
+      }
+    }
+    while (1) {
+      auto it = vis.lower_bound({mn, mx});
+      if (it != vis.begin() and prev(it)->second >= mn) {
+        it = prev(it);
+        mn = min(mn, it->first);
+        mx = max(mx, it->second);
+        vis.erase(it);
+      } else {
+        break;
+      }
+    }
+    vis.emplace(mn, mx);
+    de = max(de, mx - mn);
+    cout << sum - de << " \n"[i + 1 == n];
+  }
 }
 
 void Solver::Run() const {
   auto tt{1};
-  // cin >> tt;
+  cin >> tt;
   while (tt--) {
     Solve();
   }
