d82856018da42c99b83134469f60c2cb8bdb7960
jack <you@example.com>
2024-04-29 17:36:28 +0800

    add fn lock solve

--- sol.cc
+++ sol.cc
@@ -26,12 +26,31 @@ class Solver {
 using ll = long long;
 
 void Solver::Solve() const {
-
+  int n;
+  cin >> n;
+  vector<ll> a(n + 1);
+  for (int i = 1; i <= n; ++i) {
+    cin >> a[i];
+    a[i] += a[i - 1];
+  }
+  for (int i = 1; i <= n; ++i) {
+    int nx = 0;
+    for (int j = 0; j <= n; ++j) {
+      if (nx == a[j]) {
+        nx += a[i];
+      }
+    }
+    if (nx == a[n] + a[i]) {
+      cout << n - a[n] / a[i] << '\n';
+      return;
+    }
+  }
+  assert(0);
 }
 
 void Solver::Run() const {
   auto tt{1};
-  // cin >> tt;
+  cin >> tt;
   while (tt--) {
     Solve();
   }
@@ -42,35 +61,3 @@ signed main() {
   solver.Main();
   return 0;
 }
-class Solution {
-public:
-    int vis[100005];
-    int medianOfUniquenessArray(vector<int>& nums) {
-        long long tot=nums.size();
-        tot=(tot+1)*tot/2;
-        long long need=(tot-1)/2;
-      //  printf("%lld\n",need);
-        int Max=nums.size()+1,Min=0;
-        for(auto it:nums)vis[it]=0;
-        while(Max>Min+1){
-            int mid=(Max+Min)/2;
-            int l=0,cnt=0;
-            long long sum=0;
-            for(int i = 0;i<nums.size();i++){
-                if(!vis[nums[i]])cnt++;
-                vis[nums[i]]++;
-                while(cnt>=mid){
-                    vis[nums[l]]--;
-                    if(vis[nums[l]]==0)cnt--;
-                    l++;
-                }
-                sum+=i-l+1;
-            }
-               for(auto it:nums)vis[it]=0;
-          //  printf("%d %d\n",mid,sum);
-            if(sum<=need)Min=mid;
-            else Max=mid;
-        }
-        return Min;
-    }
-};
