90e4f4a01292df69234bb3d4690e8031e0817926
mac <machine@test.local>
2024-05-12 20:54:10 +0800

    upd mk add std f

--- makefile
+++ makefile
@@ -5,7 +5,9 @@ dfg := -DDEBUG
 gdb := -g
 debug := -DDEBUG
 
-CXXFLAGS ?= -std=c++2a -O3 -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlong-long -Wshift-overflow -Wunknown-warning-option -Wcast-qual -Wcast-align -Wno-unused-result -Wno-sign-conversion -Wl,-stack_size,256000000
+std ?= -std=c++2a
+
+CXXFLAGS ?=  -std=c++2a -O3 -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlong-long -Wshift-overflow -Wunknown-warning-option -Wcast-qual -Wcast-align -Wno-unused-result -Wno-sign-conversion -Wl,-stack_size,256000000
 
 DEBUGFLAGS := -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fstack-protector -D_FORTIFY_SOURCE=2
 
@@ -46,14 +48,14 @@ clear:
 	rm -rf $(prog).dSYM/
 
 simple:
-	$(cc) $(dfg) $(src) -o $(prog)
+	$(cc) $(src) -o $(prog) $(std) $(dfg) 
 
 verbose:
 	$(cc) $(src) -o $(prog) $(CXXFLAGS) $(DEBUGFLAGS) $(dfg) $(gdb)  2>&1 | tee $(compile_out)
 
 $(prog): $(src)
 	rm -rf $(prog)*
-	$(cc) $(src) -o $(prog) $(CXXFLAGS) $(DEBUGFLAGS) $(dfg) $(gdb)  2>&1 | tee $(compile_out)
+	$(cc) $(src) -o $(prog) $(std) $(dfg) 
 
 main: $(prog)
 	cat $(input)
--- sol.cc
+++ sol.cc
@@ -25,8 +25,60 @@ class Solver {
 
 using ll = long long;
 
-void Solver::Solve() const {
+class Solution {
+public:
+    vector<int> findPermutation(vector<int>& v) {
+        const int n = v.size();
+        const int inf = 1e9;
+        vector<vector<vector<int>>> dp(1 << n, vector<vector<int>>(n, vector<int>(n, -1)));
+        vector<vector<vector<int>>> pre(1 << n, vector<vector<int>>(n, vector<int>(n, -1)));
+        function<int(int, int, int)> Dfs = [&](int k, int st, int ed) -> int {
+            if (__builtin_popcount(k) == n) {
+              return abs(ed - v[st]);
+            }
+            if (~dp[k][st][ed]) {
+              return dp[k][st][ed];
+            }
+            int ans = inf;
+            for (int i = 0; i < n; ++i) {
+              if (k >> i & 1) {
+                continue;
+              }
+              int kk = k | 1 << i;
+              int t = abs(ed - v[i]) + Dfs(kk, st, i);
+              if (t < ans) {
+                ans = t;
+                pre[k][st][ed] = i;
+              }
+            }
+            return dp[k][st][ed] = ans;
+        };
+        int st = 0;
+        int mn = inf;
+        for (int i = 0; i < n; ++i) {
+            int t = Dfs(1 << i, i, i);
+            if (t < mn) {
+                mn = t;
+                st = i;
+            }
+        }
+        vector<int> ans{st};
+        int now = 1 << st;
+        int ed = st;
+        while (ans.size() != n) {
+          int x = pre[now][st][ed];
+          ans.push_back(x);
+          now |= 1 << x;
+          ed = x;
+        }
+        return ans;
+    }
+};
 
+void Solver::Solve() const {
+  Solution s;
+  vector<int> v{3,8,5,7,10,13,9,4,11,6,2,0,12,1};
+  debug(s.findPermutation(v));
 }
 
 void Solver::Run() const {
