191b06e225c935a7e499ec35b7783bcc44d21a2d
jack <you@example.com>
2024-05-17 14:50:53 +0800

    add sublime_key_bind

--- last.cc
+++ last.cc
@@ -16,57 +16,27 @@ class Solver {
   }
 
  private:
-  inline void SetIO() const {
-    cin.tie(0)->sync_with_stdio(0);
+  inline void SetIO() const { 
+    cin.tie(0)->sync_with_stdio(0); 
   }
   void Solve() const;
   void Run() const;
 };
 
 using ll = long long;
-const int inf = 1e9;
-const vector<int> dirs{1, 0, -1, 0, 1};
 
 void Solver::Solve() const {
   int n;
   cin >> n;
-  vector<string> a(n);
+  vector<ll> a(3), b(3);
   for (auto& i : a) {
     cin >> i;
   }
-  function<int(int, int, char)> D = [&](int s, int t, char c) -> int {
-    set<pair<int, int>> mn;
-    vector<int> dis(n * n, inf);
-    mn.emplace(0, s);
-    dis[s] = 0;
-    while (mn.size()) {
-      auto [d, u] = *mn.begin();
-      mn.erase(mn.begin());
-      if (d > dis[u]) {
-        continue;
-      }
-      int x = u / n;
-      int y = u % n;
-      for (int i = 0; i < 4; ++i) {
-        int nx = x + dirs[i];
-        int ny = y + dirs[i + 1];
-        if (nx < 0 or ny < 0 or nx >= n or ny >= n) {
-          continue;
-        }
-        int nd = d;
-        int v = nx * n + ny;
-        if (a[nx][ny] == c) {
-          ++nd;          
-        } 
-        if (dis[v] > nd) {
-          mn.emplace(nd, v);
-          dis[v] = nd;
-        }
-      }
-    }
-    return dis[t];
-  };
-  cout << D(0, n * n - 1, 'B') + D(n - 1, n * (n - 1), 'R') << '\n'; 
+  for (auto& i : b) {
+    cin >> i;
+  }
+  cout << max({0ll, a[0] + b[1] - n, a[1] + b[2] - n, a[2] + b[0] - n}) << ' ';
+  cout << min(a[0], b[1]) + min(a[1], b[2]) + min(a[2], b[0]) << '\n';
 }
 
 void Solver::Run() const {
