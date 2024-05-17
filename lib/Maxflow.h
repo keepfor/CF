e7d43da834eda9802263b7e6abaf64cbdfcf651b
jack <you@example.com>
2024-05-17 22:26:20 +0800

    upd  MaxFlow

--- lib/Maxflow.h
+++ lib/Maxflow.h
@@ -1,112 +1,157 @@
-#include <stdio.h>
-#include <vector>
-#include <queue>
-#include <limits>
-#include <string.h>
-#include <functional>
-#include <algorithm>
+#include <bits/stdc++.h>
+
 using namespace std;
-const int MAXN = 205;
-//total # of nodes
-struct Maxflow {
-	struct E {
-		int x, inv, cap, flow;
-		E(int t, int c, int z) : x(t), inv(z), cap(c), flow(0) {}
-		int rest() const { return cap - flow; }
-	};
-	int V, d[MAXN], e_try[MAXN];
-	vector <E> g[MAXN];
-	Maxflow(int V) : V(V) {}
-	void addedge(int a, int b, int a2b, int b2a = 0) {
-		int as = g[a].size(), bs = g[b].size();
-		g[a].push_back({ b, a2b, bs });
-		g[b].push_back({ a, b2a, as });
-	}
-	bool bfs(int S, int T) {
-		memset(d, -1, sizeof(d));
-		queue <int> q; q.push(S);
-		d[S] = 0;
-		while (!q.empty()) {
-			int x = q.front(); q.pop();
-			for (auto &e : g[x]) {
-				if (d[e.x] == -1 && e.rest() > 0) {
-					d[e.x] = d[x] + 1;
-					q.push(e.x);
-				}
-			}
-		}
-		return d[T] != -1;
-	}
-	int dfs(int x, int T, int flow) {
-		if (x == T) return flow;
-		for (int& i = e_try[x]; i < g[x].size(); i++) {
-			E &e = g[x][i];
-			if (e.rest() > 0 && d[e.x] == d[x] + 1) {
-				int t = dfs(e.x, T, min(flow, e.rest()));
-				if (t > 0) {
-					E &re = g[e.x][e.inv];
-					e.flow += t;
-					re.flow = -e.flow;
-					return t;
-				}
-			}
-		}
-		return 0;
-	}
-	int go(int S, int T) {
-		int res = 0, t = 0;
-		while (bfs(S, T)) {
-			memset(e_try, 0, sizeof(e_try));
-			do {
-				t = dfs(S, T, numeric_limits<int>::max());
-				res += t;
-			} while (t);
-		}
-		return res;
-	}
+
+#ifdef DEBUG
+#include "lib/debug.h"
+#else
+#define debug(...)
+#endif
+
+class Solver {
+ public:
+  inline void Main() const {
+    SetIO();
+    Run();
+  }
+
+ private:
+  inline void SetIO() const { 
+    cin.tie(0)->sync_with_stdio(0); 
+  }
+  void Solve() const;
+  void Run() const;
 };
-const int inf = 1e6;
-int n, m;
-int a[MAXN], b[MAXN], ans[MAXN][MAXN];
-int main() {
-	scanf("%d%d", &n, &m);
-	Maxflow f(2 * n + 2);
-	int as = 0, bs = 0;
-	for (int i = 1; i <= n; i++) {
-		scanf("%d", &a[i]);
-		f.addedge(0, i, a[i]);
-		as += a[i];
-	}
-	for (int i = 1; i <= n; i++) {
-		scanf("%d", &b[i]);
-		f.addedge(n + i, 2 * n + 1, b[i]);
-		bs += b[i];
-	}
-	if (as != bs) {
-		puts("NO");
-		return 0;
-	}
-	for (int i = 1; i <= m; i++) {
-		int x, y; scanf("%d%d", &x, &y);
-		f.addedge(x, n + y, inf);
-		f.addedge(y, n + x, inf);
-	}
-	for (int i = 1; i <= n; i++) {
-		f.addedge(i, n + i, a[i]);
-	}
-	int flow = f.go(0, 2 * n + 1);
-	if (flow == as) {
-		puts("YES");
-		for (int i = 1; i <= n; i++) {
-			for (auto &e : f.g[i]) {
-				int j = e.x, w = e.flow;
-				if (n + 1 <= j && j <= 2 * n) ans[i][j - n] = w;
-			}
-		}
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) printf("%d ", ans[i][j]);
-			puts("");
-		}
-	}
-	else puts("NO");
+
+template<int MAXN>
+class MaxFlow {
+ public: 
+  class E {
+   public:
+    int x, inv, cap, flow;
+    E(int t, int c, int z) : x(t), inv(z), cap(c), flow(0) {}
+    int rest() const { return cap - flow; }
+  };
+
+  vector <E> g[MAXN];
+  MaxFlow(int v_) : v(v_) {}
+  void addEdge(int a, int b, int ab, int ba = 0) {
+    int as = g[a].size(), bs = g[b].size();
+    g[a].push_back({ b, ab, bs });
+    g[b].push_back({ a, ba, as });
+  }
+  
+  int go(int s, int t) {
+    int res = 0, tmp = 0;
+    while (bfs(s, t)) {
+      memset(e_try, 0, sizeof(e_try));
+      do {
+        tmp = dfs(s, t, numeric_limits<int>::max());
+        res += tmp;
+      } while (tmp);
+    }
+    return res;
+  }
+ private:
+  int v, d[MAXN], e_try[MAXN];
+  bool bfs(int s, int t) {
+    memset(d, -1, sizeof(d));
+    queue <int> q; q.push(s);
+    d[s] = 0;
+    while (!q.empty()) {
+      int x = q.front(); q.pop();
+      for (auto &e : g[x]) {
+        if (d[e.x] == -1 && e.rest() > 0) {
+          d[e.x] = d[x] + 1;
+          q.push(e.x);
+        }
+      }
+    }
+    return d[t] != -1;
+  }
+  int dfs(int x, int t, int flow) {
+    if (x == t) return flow;
+    for (int& i = e_try[x]; i < g[x].size(); i++) {
+      E &e = g[x][i];
+      if (e.rest() > 0 && d[e.x] == d[x] + 1) {
+        int tmp = dfs(e.x, t, min(flow, e.rest()));
+        if (tmp > 0) {
+          E &re = g[e.x][e.inv];
+          e.flow += tmp;
+          re.flow = -e.flow;
+          return tmp;
+        }
+      }
+    }
+    return 0;
+  }
+};
+
+using ll = long long;
+const int inf = 1e9;
+
+void Solver::Solve() const {
+  int n, m;
+  cin >> n >> m;
+  MaxFlow<205> mf(2 * n + 2);
+  int as = 0;
+  int bs = 0;
+  for (int i = 1; i <= n; ++i) {
+    int a;
+    cin >> a;
+    as += a;
+    mf.addEdge(0, i, a);
+    mf.addEdge(i, i + n, a);
+  } 
+  for (int i = 1; i <= n; ++i) {
+    int b;
+    cin >> b;
+    bs += b;
+    mf.addEdge(i + n, 2 * n + 1, b);
+  }
+  if (as != bs) {
+    cout << "NO\n";
+    return;
+  }
+  while (m--) {
+    int p, q;
+    cin >> p >> q;
+    mf.addEdge(p, q + n, inf);    
+    mf.addEdge(q, p + n, inf);    
+  }
+  int x = mf.go(0, 2 * n + 1);
+  if (x == as and as == bs) {
+    cout << "YES\n";
+    vector<vector<int>> ans(n + 1, vector<int>(n + 1));
+    for (int i = 1; i <= n; ++i) {
+      for (auto& j : mf.g[i]) {
+        int b = j.x;
+        int w = j.flow;
+        if (n + 1 <= b and b <= 2 * n) {
+          ans[i][b - n] = w;
+        }
+      }
+    }
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        cout << ans[i][j] << " \n"[j == n];
+      }
+    }
+    return;
+  } 
+  cout << "NO\n";
+}
+
+void Solver::Run() const {
+  auto tt{1};
+  // cin >> tt;
+  while (tt--) {
+    Solve();
+  }
+}
+
+signed main() {
+  Solver solver;
+  solver.Main();
+  return 0;
 }
--- sol.cc
+++ sol.cc
@@ -23,226 +23,128 @@ class Solver {
   void Run() const;
 };
 
-using ll = long long;
+template<int MAXN>
+class MaxFlow {
+ public: 
+  class E {
+   public:
+    int x, inv, cap, flow;
+    E(int t, int c, int z) : x(t), inv(z), cap(c), flow(0) {}
+    int rest() const { return cap - flow; }
+  };
 
-struct DSU {
-  std::vector<int> f, siz, mn, mx;
-  DSU(int n) : f(n), siz(n, 1) {
-    std::iota(f.begin(), f.end(), 0), mn = mx = f;
+  vector <E> g[MAXN];
+  MaxFlow(int v_) : v(v_) {}
+  void addEdge(int a, int b, int ab, int ba = 0) {
+    int as = g[a].size(), bs = g[b].size();
+    g[a].push_back({ b, ab, bs });
+    g[b].push_back({ a, ba, as });
   }
-  int leader(int x) {
-    while (x != f[x]) x = f[x] = f[f[x]];
-    return x;
-  }
-  bool same(int x, int y) { return leader(x) == leader(y); }
-  bool merge(int x, int y) {
-    x = leader(x);
-    y = leader(y);
-    if (x == y) return false;
-    siz[x] += siz[y];
-    mn[x] = std::min(mn[x], mn[y]);
-    mx[x] = std::max(mx[x], mx[y]);
-    f[y] = x;
-    return true;
-  }
-  int size(int x) { return siz[leader(x)]; }
-  int min(int x) { return mn[leader(x)]; }
-  int max(int x) { return mx[leader(x)]; }
-};
-
-template <typename T>
-T mod_inv_in_range(T a, T m) {
-  // assert(0 <= a && a < m);
-  T x = a, y = m;
-  T vx = 1, vy = 0;
-  while (x) {
-    T k = y / x;
-    y %= x;
-    vy -= k * vx;
-    std::swap(x, y);
-    std::swap(vx, vy);
-  }
-  assert(y == 1);
-  return vy < 0 ? m + vy : vy;
-}
-
-template <typename T>
-T mod_inv(T a, T m) {
-  a %= m;
-  a = a < 0 ? a + m : a;
-  return mod_inv_in_range(a, m);
-}
-
-template <int MOD_>
-struct modnum {
-  static constexpr int MOD = MOD_;
-  static_assert(MOD_ > 0, "MOD must be positive");
-
- private:
-  int v;
-
- public:
-  modnum() : v(0) {}
-  modnum(int64_t v_) : v(int(v_ % MOD)) {
-    if (v < 0) v += MOD;
-  }
-  explicit operator int() const { return v; }
-  friend std::ostream& operator<<(std::ostream& out, const modnum& n) {
-    return out << int(n);
-  }
-  friend std::istream& operator>>(std::istream& in, modnum& n) {
-    int64_t v_;
-    in >> v_;
-    n = modnum(v_);
-    return in;
-  }
-
-  friend bool operator==(const modnum& a, const modnum& b) {
-    return a.v == b.v;
-  }
-  friend bool operator!=(const modnum& a, const modnum& b) {
-    return a.v != b.v;
-  }
-
-  modnum inv() const {
-    modnum res;
-    res.v = mod_inv_in_range(v, MOD);
-    return res;
-  }
-  friend modnum inv(const modnum& m) { return m.inv(); }
-  modnum neg() const {
-    modnum res;
-    res.v = v ? MOD - v : 0;
-    return res;
-  }
-  friend modnum neg(const modnum& m) { return m.neg(); }
-
-  modnum operator-() const { return neg(); }
-  modnum operator+() const { return modnum(*this); }
-
-  modnum& operator++() {
-    v++;
-    if (v == MOD) v = 0;
-    return *this;
-  }
-  modnum& operator--() {
-    if (v == 0) v = MOD;
-    v--;
-    return *this;
-  }
-  modnum& operator+=(const modnum& o) {
-    v -= MOD - o.v;
-    v = (v < 0) ? v + MOD : v;
-    return *this;
-  }
-  modnum& operator-=(const modnum& o) {
-    v -= o.v;
-    v = (v < 0) ? v + MOD : v;
-    return *this;
-  }
-  modnum& operator*=(const modnum& o) {
-    v = int(int64_t(v) * int64_t(o.v) % MOD);
-    return *this;
-  }
-  modnum& operator/=(const modnum& o) { return *this *= o.inv(); }
-
-  friend modnum operator++(modnum& a, int) {
-    modnum r = a;
-    ++a;
-    return r;
-  }
-  friend modnum operator--(modnum& a, int) {
-    modnum r = a;
-    --a;
-    return r;
-  }
-  friend modnum operator+(const modnum& a, const modnum& b) {
-    return modnum(a) += b;
-  }
-  friend modnum operator-(const modnum& a, const modnum& b) {
-    return modnum(a) -= b;
-  }
-  friend modnum operator*(const modnum& a, const modnum& b) {
-    return modnum(a) *= b;
-  }
-  friend modnum operator/(const modnum& a, const modnum& b) {
-    return modnum(a) /= b;
-  }
-};
-
-using num = modnum<998244353>;
-
-vector<num> fact, ifact;
-
-void init() {
-  int N = 1100000;
-  fact = {1};
-  for (int i = 1; i < N; i++) fact.push_back(i * fact[i - 1]);
-  ifact.resize(N);
-  ifact.back() = 1 / fact.back();
-  for (int i = N - 1; i > 0; i--) ifact[i - 1] = i * ifact[i];
-}
-
-num ncr(int n, int k) {
-  if (k < 0 || k > n) return 0;
-  return fact[n] * ifact[k] * ifact[n - k];
-}
-
-
-num p(num x, int y) {
-  num z = 1;
-  while (y) {
-    if (y & 1) {
-      z = z * x;
+  
+  int go(int s, int t) {
+    int res = 0, tmp = 0;
+    while (bfs(s, t)) {
+      memset(e_try, 0, sizeof(e_try));
+      do {
+        tmp = dfs(s, t, numeric_limits<int>::max());
+        res += tmp;
+      } while (tmp);
     }
-    x = x * x;
-    y >>= 1;
+    return res;
   }
-  return z;
-}
+ private:
+  int v, d[MAXN], e_try[MAXN];
+  bool bfs(int s, int t) {
+    memset(d, -1, sizeof(d));
+    queue <int> q; q.push(s);
+    d[s] = 0;
+    while (!q.empty()) {
+      int x = q.front(); q.pop();
+      for (auto &e : g[x]) {
+        if (d[e.x] == -1 && e.rest() > 0) {
+          d[e.x] = d[x] + 1;
+          q.push(e.x);
+        }
+      }
+    }
+    return d[t] != -1;
+  }
+  int dfs(int x, int t, int flow) {
+    if (x == t) return flow;
+    for (int& i = e_try[x]; i < g[x].size(); i++) {
+      E &e = g[x][i];
+      if (e.rest() > 0 && d[e.x] == d[x] + 1) {
+        int tmp = dfs(e.x, t, min(flow, e.rest()));
+        if (tmp > 0) {
+          E &re = g[e.x][e.inv];
+          e.flow += tmp;
+          re.flow = -e.flow;
+          return tmp;
+        }
+      }
+    }
+    return 0;
+  }
+};
+
+using ll = long long;
+const int inf = 1e9;
 
 void Solver::Solve() const {
-  int n;
-  cin >> n;
-  vector<int> a(n), b(n);
-  for (auto& i : a) {
-    cin >> i;
-    --i;
+  int n, m;
+  cin >> n >> m;
+  MaxFlow<205> mf(2 * n + 2);
+  int as = 0;
+  int bs = 0;
+  for (int i = 1; i <= n; ++i) {
+    int a;
+    cin >> a;
+    as += a;
+    mf.addEdge(0, i, a);
+    mf.addEdge(i, i + n, a);
+  } 
+  for (int i = 1; i <= n; ++i) {
+    int b;
+    cin >> b;
+    bs += b;
+    mf.addEdge(i + n, 2 * n + 1, b);
   }
-  for (auto& i : b) {
-    cin >> i;
-    --i;
+  if (as != bs) {
+    cout << "NO\n";
+    return;
   }
-  DSU d(n);
-  for (int i = 0; i < n; ++i) {
-    d.merge(a[i], b[i]);
+  while (m--) {
+    int p, q;
+    cin >> p >> q;
+    mf.addEdge(p, q + n, inf);    
+    mf.addEdge(q, p + n, inf);    
   }
-  vector<int> cnt(n);
-  for (int i = 0; i < n; ++i) {
-    ++cnt[d.leader(a[i])];
-  }
-  for (int i = 0; i < n; ++i) {
-    if (cnt[i] > d.size(i)) {
-      cout << 0 << '\n';
-      return;
+  int x = mf.go(0, 2 * n + 1);
+  if (x == as and as == bs) {
+    cout << "YES\n";
+    vector<vector<int>> ans(n + 1, vector<int>(n + 1));
+    for (int i = 1; i <= n; ++i) {
+      for (auto& j : mf.g[i]) {
+        int b = j.x;
+        int w = j.flow;
+        if (n + 1 <= b and b <= 2 * n) {
+          ans[i][b - n] = w;
+        }
+      }
     }
-  }
-  num ans = 1;
-  int g = 0;
-  for (int i = 0; i < n; ++i) {
-    g += d.leader(i) == i;
-    if (a[i] == b[i]) {
-      ans *= n;
-      --g;
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        cout << ans[i][j] << " \n"[j == n];
+      }
     }
-  }
-  ans *= p(num(2), g);
-  cout << ans << '\n';
+    return;
+  } 
+  cout << "NO\n";
 }
 
 void Solver::Run() const {
   auto tt{1};
-  cin >> tt;
+  // cin >> tt;
   while (tt--) {
     Solve();
   }
