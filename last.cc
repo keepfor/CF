4302f26e667cc47118a3659f1a57490d9b7be515
jack <you@example.com>
2024-05-30 20:56:03 +0800

    ad inversion.h

--- last.cc
+++ last.cc
@@ -16,8 +16,8 @@ class Solver {
   }
 
  private:
-  inline void SetIO() const { 
-    cin.tie(0)->sync_with_stdio(0); 
+  inline void SetIO() const {
+    cin.tie(0)->sync_with_stdio(0);
   }
   void Solve() const;
   void Run() const;
@@ -28,7 +28,30 @@ using ll = long long;
 void Solver::Solve() const {
   int n;
   cin >> n;
-  cout << string(n, char('0' + n)) << '\n';
+  vector<vector<int>> g(n);
+  for (int i = 1; i < n; ++i) {
+    int u, v;
+    cin >> u >> v;
+    --u;
+    --v;
+    g[u].push_back(v);
+    g[v].push_back(u);
+  }
+  vector<int> sz(n, 1);
+  auto D = [&](auto&& self, int u, int pa) -> void {
+    for (auto& v : g[u]) {
+      if (v != pa) {
+        self(self, v, u);
+        sz[u] += sz[v];
+      }
+    }
+  };
+  D(D, 0, -1);
+  int ans = n;
+  for (auto& v : g[0]) {
+    ans = min(ans, n - sz[v]);
+  }
+  cout << ans << '\n';
 }
 
 void Solver::Run() const {
