73418800e53fd6a399b9cb8899c17006b0aa63da
jack <you@example.com>
2024-08-19 21:53:47 +0800

    BinaryLifting

---
+++ lib/BinaryLifting.h
@@ -0,0 +1,150 @@
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <deque>
+#include <forward_list>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+using lint = long long;
+using pint = pair<int, int>;
+using plint = pair<lint, lint>;
+struct fast_ios { fast_ios(){ cin.tie(nullptr), ios::sync_with_stdio(false), cout << fixed << setprecision(20); }; } fast_ios_;
+#define ALL(x) (x).begin(), (x).end()
+#define FOR(i, begin, end) for(int i=(begin),i##_end_=(end);i<i##_end_;i++)
+#define IFOR(i, begin, end) for(int i=(end)-1,i##_begin_=(begin);i>=i##_begin_;i--)
+#define REP(i, n) FOR(i,0,n)
+#define IREP(i, n) IFOR(i,0,n)
+template <typename T> bool chmax(T &m, const T q) { return m < q ? (m = q, true) : false; }
+template <typename T> bool chmin(T &m, const T q) { return m > q ? (m = q, true) : false; }
+const std::vector<std::pair<int, int>> grid_dxs{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+int floor_lg(long long x) { return x <= 0 ? -1 : 63 - __builtin_clzll(x); }
+template <class T1, class T2> T1 floor_div(T1 num, T2 den) { return (num > 0 ? num / den : -((-num + den - 1) / den)); }
+template <class T1, class T2> std::pair<T1, T2> operator+(const std::pair<T1, T2> &l, const std::pair<T1, T2> &r) { return std::make_pair(l.first + r.first, l.second + r.second); }
+template <class T1, class T2> std::pair<T1, T2> operator-(const std::pair<T1, T2> &l, const std::pair<T1, T2> &r) { return std::make_pair(l.first - r.first, l.second - r.second); }
+template <class T> std::vector<T> sort_unique(std::vector<T> vec) { sort(vec.begin(), vec.end()), vec.erase(unique(vec.begin(), vec.end()), vec.end()); return vec; }
+template <class T> int arglb(const std::vector<T> &v, const T &x) { return std::distance(v.begin(), std::lower_bound(v.begin(), v.end(), x)); }
+template <class T> int argub(const std::vector<T> &v, const T &x) { return std::distance(v.begin(), std::upper_bound(v.begin(), v.end(), x)); }
+template <class IStream, class T> IStream &operator>>(IStream &is, std::vector<T> &vec) { for (auto &v : vec) is >> v; return is; }
+
+template <class OStream, class T> OStream &operator<<(OStream &os, const std::vector<T> &vec);
+template <class OStream, class T, size_t sz> OStream &operator<<(OStream &os, const std::array<T, sz> &arr);
+template <class OStream, class T, class TH> OStream &operator<<(OStream &os, const std::unordered_set<T, TH> &vec);
+template <class OStream, class T, class U> OStream &operator<<(OStream &os, const pair<T, U> &pa);
+template <class OStream, class T> OStream &operator<<(OStream &os, const std::deque<T> &vec);
+template <class OStream, class T> OStream &operator<<(OStream &os, const std::set<T> &vec);
+template <class OStream, class T> OStream &operator<<(OStream &os, const std::multiset<T> &vec);
+template <class OStream, class T> OStream &operator<<(OStream &os, const std::unordered_multiset<T> &vec);
+template <class OStream, class T, class U> OStream &operator<<(OStream &os, const std::pair<T, U> &pa);
+template <class OStream, class TK, class TV> OStream &operator<<(OStream &os, const std::map<TK, TV> &mp);
+template <class OStream, class TK, class TV, class TH> OStream &operator<<(OStream &os, const std::unordered_map<TK, TV, TH> &mp);
+template <class OStream, class... T> OStream &operator<<(OStream &os, const std::tuple<T...> &tpl);
+
+template <class OStream, class T> OStream &operator<<(OStream &os, const std::vector<T> &vec) { os << '['; for (auto v : vec) os << v << ','; os << ']'; return os; }
+template <class OStream, class T, size_t sz> OStream &operator<<(OStream &os, const std::array<T, sz> &arr) { os << '['; for (auto v : arr) os << v << ','; os << ']'; return os; }
+template <class... T> std::istream &operator>>(std::istream &is, std::tuple<T...> &tpl) { std::apply([&is](auto &&... args) { ((is >> args), ...);}, tpl); return is; }
+template <class OStream, class... T> OStream &operator<<(OStream &os, const std::tuple<T...> &tpl) { os << '('; std::apply([&os](auto &&... args) { ((os << args << ','), ...);}, tpl); return os << ')'; }
+template <class OStream, class T, class TH> OStream &operator<<(OStream &os, const std::unordered_set<T, TH> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <class OStream, class T> OStream &operator<<(OStream &os, const std::deque<T> &vec) { os << "deq["; for (auto v : vec) os << v << ','; os << ']'; return os; }
+template <class OStream, class T> OStream &operator<<(OStream &os, const std::set<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <class OStream, class T> OStream &operator<<(OStream &os, const std::multiset<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <class OStream, class T> OStream &operator<<(OStream &os, const std::unordered_multiset<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <class OStream, class T, class U> OStream &operator<<(OStream &os, const std::pair<T, U> &pa) { return os << '(' << pa.first << ',' << pa.second << ')'; }
+template <class OStream, class TK, class TV> OStream &operator<<(OStream &os, const std::map<TK, TV> &mp) { os << '{'; for (auto v : mp) os << v.first << "=>" << v.second << ','; os << '}'; return os; }
+template <class OStream, class TK, class TV, class TH> OStream &operator<<(OStream &os, const std::unordered_map<TK, TV, TH> &mp) { os << '{'; for (auto v : mp) os << v.first << "=>" << v.second << ','; os << '}'; return os; }
+#ifdef HITONANODE_LOCAL
+const string COLOR_RESET = "\033[0m", BRIGHT_GREEN = "\033[1;32m", BRIGHT_RED = "\033[1;31m", BRIGHT_CYAN = "\033[1;36m", NORMAL_CROSSED = "\033[0;9;37m", RED_BACKGROUND = "\033[1;41m", NORMAL_FAINT = "\033[0;2m";
+#define dbg(x) std::cerr << BRIGHT_CYAN << #x << COLOR_RESET << " = " << (x) << NORMAL_FAINT << " (L" << __LINE__ << ") " << __FILE__ << COLOR_RESET << std::endl
+#define dbgif(cond, x) ((cond) ? std::cerr << BRIGHT_CYAN << #x << COLOR_RESET << " = " << (x) << NORMAL_FAINT << " (L" << __LINE__ << ") " << __FILE__ << COLOR_RESET << std::endl : std::cerr)
+#else
+#define dbg(x) ((void)0)
+#define dbgif(cond, x) ((void)0)
+#endif
+
+#include <cstdlib>
+#include <vector>
+
+// Binary lifting / `Doubling`
+// Complexity: O(NlogN) precalculation / O(logN) per query
+// <https://atcoder.jp/contests/arc060/submissions/7039451>
+struct BinaryLifting {
+    int N, INVALID, lgD;
+    std::vector<std::vector<int>> mat;
+    BinaryLifting() : N(0), lgD(0) {}
+    BinaryLifting(const std::vector<int> &vec_nxt, int INVALID = -1, int lgd = 0)
+        : N(vec_nxt.size()), INVALID(INVALID), lgD(lgd) {
+        while ((1LL << lgD) < N) lgD++;
+        mat.assign(lgD, std::vector<int>(N, INVALID));
+        mat[0] = vec_nxt;
+        for (int i = 0; i < N; i++)
+            if (mat[0][i] < 0 or mat[0][i] >= N) mat[0][i] = INVALID;
+        for (int d = 0; d < lgD - 1; d++) {
+            for (int i = 0; i < N; i++)
+                if (mat[d][i] != INVALID) mat[d + 1][i] = mat[d][mat[d][i]];
+        }
+    }
+    int kth_next(int now, long long k) {
+        if (k >= (1LL << lgD)) exit(8);
+        for (int d = 0; k and now != INVALID; d++, k >>= 1)
+            if (k & 1) now = mat[d][now];
+        return now;
+    }
+
+    // Distance from l to [r, \infty)
+    // Requirement: mat[0][i] > i for all i (monotone increasing)
+    int distance(int l, int r) {
+        if (l >= r) return 0;
+        int ret = 0;
+        for (int d = lgD - 1; d >= 0; d--) {
+            if (mat[d][l] < r and mat[d][l] != INVALID) ret += 1 << d, l = mat[d][l];
+        }
+        if (mat[0][l] == INVALID or mat[0][l] >= r)
+            return ret + 1;
+        else
+            return -1; // Unable to reach
+    }
+};
+
+
+int main() {
+    int N;
+    lint K;
+    cin >> N >> K;
+    vector<int> X(N), A(N);
+    cin >> X >> A;
+    for (auto &x : X) --x;
+
+    vector<int> nxt(N);
+    REP(i, N) nxt.at(i) = X.at(i);
+    BinaryLifting bl(nxt, -1, 60);
+    REP(i, N) {
+        cout << A.at(bl.kth_next(i, K)) << ' ';
+    }
+    cout << endl;
+}
