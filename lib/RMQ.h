1739be10b5f82a28945d7d92936b5cacd325bb67
mac <machine@test.local>
2024-09-10 14:44:03 +0800

    upd rmq

--- lib/RMQ.h
+++ lib/RMQ.h
@@ -1,9 +1,11 @@
+
 #include <algorithm>
 #include <array>
 #include <bitset>
 #include <cassert>
 #include <chrono>
 #include <cmath>
+#include <cstdint>
 #include <cstring>
 #include <functional>
 #include <iomanip>
@@ -17,96 +19,163 @@
 using namespace std;
 
 // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
-template <class Fun>
-class y_combinator_result {
-  Fun fun_;
-
- public:
-  template <class T>
-  explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
-  template <class... Args>
-  decltype(auto) operator()(Args &&...args) {
-    return fun_(std::ref(*this), std::forward<Args>(args)...);
-  }
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
 };
-template <class Fun>
-decltype(auto) y_combinator(Fun &&fun) {
-  return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
-}
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
 
-template <typename A, typename B>
-ostream &operator<<(ostream &os, const pair<A, B> &p) {
-  return os << '(' << p.first << ", " << p.second << ')';
-}
-template <typename T_container, typename T = typename enable_if<
-                                    !is_same<T_container, string>::value,
-                                    typename T_container::value_type>::type>
-ostream &operator<<(ostream &os, const T_container &v) {
-  os << '{';
-  string sep;
-  for (const T &x : v) os << sep << x, sep = ", ";
-  return os << '}';
-}
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename... Args> ostream& operator<<(ostream& os, const tuple<Args...>& t) { os << '('; apply([&os](const Args&... args) { size_t n = 0; ((os << args << (++n != sizeof...(Args) ? ", " : "")), ...); }, t); return os << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
 
 void dbg_out() { cerr << endl; }
-template <typename Head, typename... Tail>
-void dbg_out(Head H, Tail... T) {
-  cerr << ' ' << H;
-  dbg_out(T...);
-}
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
 #ifdef NEAL_DEBUG
-#define dbg(...)                                                               \
-  cerr << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", \
-      dbg_out(__VA_ARGS__)
+#define dbg(...) cerr << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 #else
 #define dbg(...)
 #endif
 
-// Warning: this uses nearly 170 MB for N = 500,000. If low on memory use
-// memory_rmq_lca.cc or block_rmq_lca.cc instead
-template <typename T, bool maximum_mode = false>
+// Warning: LCA uses nearly 170 MB for N = 500,000. If low on memory use memory_rmq_lca.cc or block_rmq_lca.cc instead.
+template<typename T, bool maximum_mode = false>
 struct RMQ {
-  static int highest_bit(unsigned x) {
-    return x == 0 ? -1 : 31 - __builtin_clz(x);
-  }
+    static int highest_bit(unsigned x) {
+        return x == 0 ? -1 : 31 - __builtin_clz(x);
+    }
 
-  int n = 0;
-  vector<T> values;
-  vector<vector<int>> range_low;
+    int n = 0;
+    vector<T> values;
+    vector<vector<int>> range_low;
 
-  RMQ(const vector<T> &_values = {}) {
-    if (!_values.empty()) build(_values);
-  }
+    RMQ(const vector<T> &_values = {}) {
+        if (!_values.empty())
+            build(_values);
+    }
 
-  // Note: when `values[a] == values[b]`, returns b.
-  int better_index(int a, int b) const {
-    return (maximum_mode ? values[b] < values[a] : values[a] < values[b]) ? a
-                                                                          : b;
-  }
+    // Note: when `values[a] == values[b]`, returns b.
+    int better_index(int a, int b) const {
+        return (maximum_mode ? values[b] < values[a] : values[a] < values[b]) ? a : b;
+    }
 
-  void build(const vector<T> &_values) {
-    values = _values;
-    n = int(values.size());
-    int levels = highest_bit(n) + 1;
-    range_low.resize(levels);
+    void build(const vector<T> &_values) {
+        values = _values;
+        n = int(values.size());
+        int levels = highest_bit(n) + 1;
+        range_low.resize(levels);
 
-    for (int k = 0; k < levels; k++) range_low[k].resize(n - (1 << k) + 1);
+        for (int k = 0; k < levels; k++)
+            range_low[k].resize(n - (1 << k) + 1);
 
-    for (int i = 0; i < n; i++) range_low[0][i] = i;
+        for (int i = 0; i < n; i++)
+            range_low[0][i] = i;
 
-    for (int k = 1; k < levels; k++)
-      for (int i = 0; i <= n - (1 << k); i++)
-        range_low[k][i] = better_index(range_low[k - 1][i],
-                                       range_low[k - 1][i + (1 << (k - 1))]);
-  }
+        for (int k = 1; k < levels; k++)
+            for (int i = 0; i <= n - (1 << k); i++)
+                range_low[k][i] = better_index(range_low[k - 1][i], range_low[k - 1][i + (1 << (k - 1))]);
+    }
 
-  // Note: breaks ties by choosing the largest index.
-  int query_index(int a, int b) const {
-    assert(0 <= a && a < b && b <= n);
-    int level = highest_bit(b - a);
-    return better_index(range_low[level][a],
-                        range_low[level][b - (1 << level)]);
-  }
+    // Note: breaks ties by choosing the largest index.
+    int query_index(int a, int b) const {
+        assert(0 <= a && a < b && b <= n);
+        int level = highest_bit(b - a);
+        return better_index(range_low[level][a], range_low[level][b - (1 << level)]);
+    }
 
-  T query_value(int a, int b) const { return values[query_index(a, b)]; }
+    T query_value(int a, int b) const {
+        return values[query_index(a, b)];
+    }
 };
+
+// For every i, finds the smallest j > i such that `compare(values[j], values[i])` is true, or `n` if no such j exists.
+template<typename T, typename T_compare>
+vector<int> closest_right(const vector<T> &values, T_compare &&compare) {
+    int n = int(values.size());
+    vector<int> closest(n);
+    vector<int> stack;
+
+    for (int i = n - 1; i >= 0; i--) {
+        while (!stack.empty() && !compare(values[stack.back()], values[i]))
+            stack.pop_back();
+
+        closest[i] = stack.empty() ? n : stack.back();
+        stack.push_back(i);
+    }
+
+    return closest;
+}
+
+
+void run_case() {
+    int N, K, Q;
+    cin >> N >> K >> Q;
+    vector<int> A(N);
+
+    for (auto &a : A)
+        cin >> a;
+
+    for (int i = 0; i < N; i++)
+        A[i] += N - 1 - i;
+
+    vector<int> freq(2 * N, 0);
+    vector<int> ans(N - (K - 1), -1);
+    multiset<int> freq_set;
+
+    auto change = [&](int x, int delta) -> void {
+        auto it = freq_set.find(freq[x]);
+
+        if (it != freq_set.end())
+            freq_set.erase(it);
+
+        freq[x] += delta;
+        freq_set.insert(freq[x]);
+    };
+
+    for (int i = 0; i < N; i++) {
+        change(A[i], 1);
+
+        if (i >= K - 1) {
+            ans[i - (K - 1)] = K - *prev(freq_set.end());
+            change(A[i - (K - 1)], -1);
+        }
+    }
+
+    RMQ<int> rmq(ans);
+    vector<int> right = closest_right(ans, less<int>());
+    vector<int64_t> suffix_sum(N + 1, 0);
+
+    for (int i = N - K; i >= 0; i--)
+        suffix_sum[i] = suffix_sum[right[i]] + int64_t(right[i] - i) * ans[i];
+
+    dbg(ans);
+    dbg(suffix_sum);
+
+    auto query = [&](int L, int R) -> int64_t {
+        int len = R - L - (K - 1);
+        int min_index = rmq.query_index(L, L + len);
+        return suffix_sum[L] - suffix_sum[min_index] + int64_t(L + len - min_index) * ans[min_index];
+    };
+
+    for (int q = 0; q < Q; q++) {
+        int L, R;
+        cin >> L >> R;
+        L--;
+        cout << query(L, R) << '\n';
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+    cin.tie(nullptr);
+#endif
+
+    int tests;
+    cin >> tests;
+
+    while (tests-- > 0)
+        run_case();
+}
