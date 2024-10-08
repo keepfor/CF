d25220e67c2c6b730f415836754127052ad1c640
mac-strong <machine@test.local>
2024-10-08 19:19:07 +0800

    add thread

---
+++ lib/instance.h
@@ -0,0 +1,304 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sys/time.h>
+#include <thread>
+#include <vector>
+using namespace std;
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename... Args> ostream& operator<<(ostream& os, const tuple<Args...>& t) { os << '('; apply([&os](const Args&... args) { size_t n = 0; ((os << args << (++n != sizeof...(Args) ? ", " : "")), ...); }, t); return os << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef NEAL_DEBUG
+#define dbg(...) cerr << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<const int &MOD>
+struct _m_int {
+    int val;
+
+    _m_int(int64_t v = 0) {
+        if (v < 0) v = v % MOD + MOD;
+        if (v >= MOD) v %= MOD;
+        val = int(v);
+    }
+
+    _m_int(uint64_t v) {
+        if (v >= MOD) v %= MOD;
+        val = int(v);
+    }
+
+    _m_int(int v) : _m_int(int64_t(v)) {}
+    _m_int(unsigned v) : _m_int(uint64_t(v)) {}
+
+    explicit operator int() const { return val; }
+    explicit operator unsigned() const { return val; }
+    explicit operator int64_t() const { return val; }
+    explicit operator uint64_t() const { return val; }
+    explicit operator double() const { return val; }
+    explicit operator long double() const { return val; }
+
+    _m_int& operator+=(const _m_int &other) {
+        val -= MOD - other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+
+    _m_int& operator-=(const _m_int &other) {
+        val -= other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+#if !defined(_WIN32) || defined(_WIN64)
+        return unsigned(x % m);
+#endif
+        // Optimized mod for Codeforces 32-bit machines.
+        // x must be less than 2^32 * m for this to work, so that x / m fits in an unsigned 32-bit int.
+        unsigned x_high = unsigned(x >> 32), x_low = unsigned(x);
+        unsigned quot, rem;
+        asm("divl %4\n"
+            : "=a" (quot), "=d" (rem)
+            : "d" (x_high), "a" (x_low), "r" (m));
+        return rem;
+    }
+
+    _m_int& operator*=(const _m_int &other) {
+        val = fast_mod(uint64_t(val) * other.val);
+        return *this;
+    }
+
+    _m_int& operator/=(const _m_int &other) {
+        return *this *= other.inv();
+    }
+
+    friend _m_int operator+(const _m_int &a, const _m_int &b) { return _m_int(a) += b; }
+    friend _m_int operator-(const _m_int &a, const _m_int &b) { return _m_int(a) -= b; }
+    friend _m_int operator*(const _m_int &a, const _m_int &b) { return _m_int(a) *= b; }
+    friend _m_int operator/(const _m_int &a, const _m_int &b) { return _m_int(a) /= b; }
+
+    _m_int& operator++() {
+        val = val == MOD - 1 ? 0 : val + 1;
+        return *this;
+    }
+
+    _m_int& operator--() {
+        val = val == 0 ? MOD - 1 : val - 1;
+        return *this;
+    }
+
+    _m_int operator++(int) { _m_int before = *this; ++*this; return before; }
+    _m_int operator--(int) { _m_int before = *this; --*this; return before; }
+
+    _m_int operator-() const {
+        return val == 0 ? 0 : MOD - val;
+    }
+
+    friend bool operator==(const _m_int &a, const _m_int &b) { return a.val == b.val; }
+    friend bool operator!=(const _m_int &a, const _m_int &b) { return a.val != b.val; }
+    friend bool operator<(const _m_int &a, const _m_int &b) { return a.val < b.val; }
+    friend bool operator>(const _m_int &a, const _m_int &b) { return a.val > b.val; }
+    friend bool operator<=(const _m_int &a, const _m_int &b) { return a.val <= b.val; }
+    friend bool operator>=(const _m_int &a, const _m_int &b) { return a.val >= b.val; }
+
+    static const int SAVE_INV = int(1e6) + 5;
+    static _m_int save_inv[SAVE_INV];
+
+    static void prepare_inv() {
+        // Ensures that MOD is prime, which is necessary for the inverse algorithm below.
+        for (int64_t p = 2; p * p <= MOD; p += p % 2 + 1)
+            assert(MOD % p != 0);
+
+        save_inv[0] = 0;
+        save_inv[1] = 1;
+
+        for (int i = 2; i < SAVE_INV; i++)
+            save_inv[i] = save_inv[MOD % i] * (MOD - MOD / i);
+    }
+
+    _m_int inv() const {
+        if (save_inv[1] == 0)
+            prepare_inv();
+
+        if (val < SAVE_INV)
+            return save_inv[val];
+
+        _m_int product = 1;
+        int v = val;
+
+        do {
+            product *= MOD - MOD / v;
+            v = MOD % v;
+        } while (v >= SAVE_INV);
+
+        return product * save_inv[v];
+    }
+
+    _m_int pow(int64_t p) const {
+        if (p < 0)
+            return inv().pow(-p);
+
+        _m_int a = *this, result = 1;
+
+        while (p > 0) {
+            if (p & 1)
+                result *= a;
+
+            p >>= 1;
+
+            if (p > 0)
+                a *= a;
+        }
+
+        return result;
+    }
+
+    friend ostream& operator<<(ostream &os, const _m_int &m) {
+        return os << m.val;
+    }
+};
+
+template<const int &MOD> _m_int<MOD> _m_int<MOD>::save_inv[_m_int<MOD>::SAVE_INV];
+
+const int MOD = 998244353;
+using mod_int = _m_int<MOD>;
+
+
+const int LEN_MAX = 105;
+vector<mod_int> p26;
+
+
+struct instance {
+    ostringstream out;
+
+    int N;
+    vector<string> S;
+
+    void input() {
+        cin >> N;
+        S.resize(N);
+
+        for (auto &str : S)
+            cin >> str;
+    }
+
+    string combine(const string &x, const string &y) {
+        size_t len = min(x.size(), y.size());
+        string str;
+
+        for (size_t i = 0; i < len; i++) {
+            if (x[i] != y[i] && x[i] != '?' && y[i] != '?')
+                return str;
+
+            str += max(x[i], y[i]);
+        }
+
+        return str;
+    }
+
+    mod_int score(const string &str) {
+        int questions = 0;
+        mod_int sum = 1;
+
+        for (auto &ch : str) {
+            questions += ch == '?';
+            sum += p26[questions];
+        }
+
+        return sum;
+    }
+
+    void solve() {
+        mod_int total = 0;
+
+        y_combinator([&](auto self, int mask, int index, const string &str) -> void {
+            if (index >= N) {
+                if (mask == 0)
+                    return;
+
+                if (__builtin_popcount(mask) % 2 == 0)
+                    total -= score(str);
+                else
+                    total += score(str);
+
+                return;
+            }
+
+            self(mask, index + 1, str);
+            self(mask | 1 << index, index + 1, combine(str, S[index]));
+        })(0, 0, string(LEN_MAX, '?'));
+
+
+        out << total << '\n';
+    }
+};
+
+
+long double time_now() {
+    timeval tv;
+    gettimeofday(&tv, nullptr);
+    return tv.tv_sec + tv.tv_usec * 1e-6L;
+}
+
+int main() {
+    p26.assign(LEN_MAX + 1, 1);
+
+    for (int len = 1; len <= LEN_MAX; len++)
+        p26[len] = p26[len - 1] * 26;
+
+    long double begin = time_now();
+    cerr << fixed << setprecision(3);
+
+    int tests;
+    cin >> tests;
+    cerr << tests << " test cases" << endl;
+    vector<instance> instances(tests + 1);
+    vector<thread> threads(tests + 1);
+
+    for (int tc = 1; tc <= tests; tc++)
+        instances[tc].input();
+
+    cerr << "Finished input (" << time_now() - begin << "s)" << endl;
+
+    for (int tc = 1; tc <= tests; tc++)
+        threads[tc] = thread(&instance::solve, &instances[tc]);
+
+    for (int tc = 1; tc <= tests; tc++) {
+        threads[tc].join();
+        string output = instances[tc].out.str();
+        cerr << "Test " << tc << " / " << tests << " complete (" << time_now() - begin << "s): " << output;
+        cout << "Case #" << tc << ": " << output;
+        cout << flush;
+        assert(!output.empty());
+    }
+}
