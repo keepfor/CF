bc56773ebfc852d8c286e62f78e7d944379f666b
mac-strong <machine@test.local>
2024-10-20 22:32:43 +0800

    upd pbds ad sol

--- lib/pbds.h
+++ lib/pbds.h
@@ -68,3 +68,104 @@ int main() {
   }
   return 0;
 }
+
+
+#include <bits/stdc++.h>
+
+class Solver {
+ public:
+  void Main() const;
+
+ private:
+  void SetIO() const;
+  void Run() const;
+  void Solve() const;
+};
+
+using namespace std;
+
+#ifdef DEBUG
+#include "lib/debug.h"
+#else
+#define debug(...)
+#endif
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef int64_t tp;
+typedef tree<tp,null_type,less<tp>,rb_tree_tag,tree_order_statistics_node_update> pbds;
+
+using ll = int64_t;
+constexpr ll inf = 4e18;
+
+void Solver::Solve() const {
+  int q;
+  ll k;
+  cin >> q >> k;
+  set<ll> st;
+  st.insert(inf);
+  pbds vis;
+  vis.insert(inf);
+  vis.insert(-inf);
+  auto Del = [&](ll l, ll r) -> void {
+    if (r - l > k) {
+      st.erase(r);
+    }
+  };
+  auto Add = [&](ll l, ll r) -> void {
+    if (r - l > k) {
+      st.insert(r);
+    }
+  };
+  while (q--) {
+    int t;
+    ll x;
+    cin >> t >> x;
+    if (t == 1) {
+      auto it = vis.find(x);
+      if (it == vis.end()) {
+        auto right = vis.upper_bound(x);
+        auto pre = *prev(right);
+        Del(pre, *right);
+        Add(pre, x);
+        Add(x, *right);
+        vis.insert(x);
+      } else {
+        auto pre = *prev(it);
+        auto nx = *next(it);
+        Del(pre, *it);
+        Del(*it, nx);
+        Add(pre, nx);
+        vis.erase(x);
+      }
+    } else {
+      auto it = st.upper_bound(x);
+      ll ans = vis.order_of_key(*it) - vis.order_of_key(*prev(it));
+      cout << ans << '\n';
+    }
+  }
+}
+
+void Solver::Run() const {
+  auto tt{1};
+  // cin >> tt;
+  while (tt--) {
+    Solve();
+  }
+}
+
+inline void Solver::SetIO() const { 
+  cin.tie(0)->sync_with_stdio(0); 
+}
+
+void Solver::Main() const {
+  SetIO();
+  Run();
+}
+
+signed main() {
+  Solver solver;
+  solver.Main();
+  return 0;
+}
